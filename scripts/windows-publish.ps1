458d9918f4b9bf882c49db300fa09e739b86aa8f
Zhouzexi <anidlebrain@qq.com>
2024-01-09 09:33:48 +0800

    修改脚本

--- scripts/windows-publish.ps1
+++ scripts/windows-publish.ps1
@@ -4,9 +4,19 @@ param (
 )
 # 外部环境变量包括:
 # archiveName: ${{ matrix.qt_ver }}-${{ matrix.qt_arch }}
+# winSdkDir: ${{ steps.build.outputs.winSdkDir }}
+# winSdkVer: ${{ steps.build.outputs.winSdkVer }}
+# vcToolsInstallDir: ${{ steps.build.outputs.vcToolsInstallDir }}
+# vcToolsRedistDir: ${{ steps.build.outputs.vcToolsRedistDir }}
+# msvcArch: ${{ matrix.msvc_arch }}
 
 
-# archiveName: 5.15.2-win64_mingw81
+# winSdkDir: C:\Program Files (x86)\Windows Kits\10\ 
+# winSdkVer: 10.0.19041.0\ 
+# vcToolsInstallDir: C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC\14.28.29333\ 
+# vcToolsRedistDir: C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Redist\MSVC\14.28.29325\ 
+# archiveName: 5.9.9-win32_msvc2015
+# msvcArch: x86
 
 $scriptDir = $PSScriptRoot
 $currentDir = Get-Location
@@ -23,6 +33,12 @@ function Main() {
     # 删除不必要的文件
     $excludeList = @("*.qmlc", "*.ilk", "*.exp", "*.lib", "*.pdb")
     Remove-Item -Path $archiveName -Include $excludeList -Recurse -Force
+    # 拷贝vcRedist dll
+    $redistDll="{0}{1}\*.CRT\*.dll" -f $env:vcToolsRedistDir.Trim(),$env:msvcArch
+    Copy-Item $redistDll $archiveName\
+    # 拷贝WinSDK dll
+    $sdkDll="{0}Redist\{1}ucrt\DLLs\{2}\*.dll" -f $env:winSdkDir.Trim(),$env:winSdkVer.Trim(),$env:msvcArch
+    Copy-Item $sdkDll $archiveName\
     # 打包zip
     Compress-Archive -Path $archiveName $archiveName'.zip'
 }
@@ -32,5 +48,3 @@ if ($null -eq $archiveName || $null -eq $targetName) {
     return
 }
 Main
-
-
