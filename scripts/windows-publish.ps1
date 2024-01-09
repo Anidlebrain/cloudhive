7da64f3a13ccbeee703293f349b14a4c236e4803
Zhouzexi <anidlebrain@qq.com>
2024-01-09 10:51:36 +0800

    Update windows-publish.ps1

--- scripts/windows-publish.ps1
+++ scripts/windows-publish.ps1
@@ -28,7 +28,7 @@ function Main() {
     New-Item -ItemType Directory $archiveName
     # 拷贝exe
     Copy-Item code\release\$targetName $archiveName\
-	Copy-Item dll\* $archiveName\
+	Copy-Item code\res $archiveName\
     # 拷贝依赖
     windeployqt --qmldir . --plugindir $archiveName\plugins --no-translations --compiler-runtime $archiveName\$targetName
     # 删除不必要的文件
